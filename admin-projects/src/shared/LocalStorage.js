b60c971d36b0b6aa2c9c582b8654da87b8e9f17d
Evenor Briceño <65525208+eve-bri@users.noreply.github.com>
2023-06-03 02:01:23 -0600

    Reestructuración
    Pasar el proyecto a typeScript

--- admin-projects/src/shared/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const setItem = async (name, value) =>{
-    await AsyncStorage.setItem(name, value);
-}
-
-export const getItem = async (name) => {
-    return await AsyncStorage.getItem(name);
-}
